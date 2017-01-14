f3013728f285b87330479fdd300e11115bfd442f
Shingo Matsui <shingo.m1217@gmail.com>
2017-01-14 13:05:41 +0900

    Update smooth-scroll.js

--- src/scripts/smooth-scroll.js
+++ src/scripts/smooth-scroll.js
@@ -1,10 +1,8 @@
-module.exports = function() {
-  $('a[href^="#"]').click(function(){
-    let speed = 500;
-    let href= $(this).attr("href");
-    let target = $(href == "#" || href == "" ? 'html' : href);
-    let position = target.offset().top;
-    $("html, body").animate({scrollTop:position}, speed, "swing");
-    return false;
-  });
-}
+$('a[href^="#"]').click(function(){
+  let speed = 500;
+  let href= $(this).attr("href");
+  let target = $(href == "#" || href == "" ? 'html' : href);
+  let position = target.offset().top;
+  $("html, body").animate({scrollTop:position}, speed, "swing");
+  return false;
+});
